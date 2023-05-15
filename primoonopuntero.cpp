1a58b754c14ff7a3251398c45c12f0a133b1c775
AlexRalston <alejandroacedo@puenteuropa.com>
2023-05-15 09:55:34 +0200

    codigo comentado

--- primoonopuntero.cpp
+++ primoonopuntero.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 
 
-bool esPrimo(int numero){
+bool esPrimo(int numero){ //funcion que devuelve true o false y que evalua un numero de tipo entero
 
 
-    if(numero <= 1){
+    if(numero <= 1){ //Condicion, si el numero es menor o igual que 1...
 
 
-        return false;
+        return false;//Devuelve false
     }
 
 
-    for(int i=2; i*i <= numero;i++){
+    for(int i=2; i*i <= numero;i++){ //Bucle que va contando desde el 2 hasta la raiz cuadrada del numero en cuestion
 
 
-        if(numero % i == 0){
+        if(numero % i == 0){ //Si se encuentra algun divisor retorna false
 
             return false;
         } 
@@ -26,27 +26,27 @@ bool esPrimo(int numero){
     }
 
 
-    return false;
+    return true; //En cualquier otro caso, retorna true.
 
 }
 
-    int main() {
-    int numero;
-    cout << "Ingrese un numero: ";
-    cin >> numero;
+    int main() { //Programa principal
+    int numero; //Numero de tipo entero
+    cout << "Ingrese un numero: "; //Salida de datos para ingresar un numero
+    cin >> numero; //El usuario introduce un numero por pantalla
 
-    int* puntero = &numero;
-    bool primo = esPrimo(*puntero);
+    int* puntero = &numero; //Asignamos una direccion de memoria a la variable puntero
+    bool primo = esPrimo(*puntero); //Comprobamos si el numero almacenado en la variable puntero es primo o no mediante una llamada
 
-    if (primo) {
-        cout << "El numero es primo." << endl;
+    if (primo) { //Si el valor de primo es true
+        cout << "El numero es primo." << endl; //Salida de datos para anunciar que es primo
     } else {
-        cout << "El numero no es primo." << endl;
+        cout << "El numero no es primo." << endl;//En cualquier otro caso, no es primo.
     }
 
-    cout << "La posicion de memoria donde se guarda el numero es: " << puntero << endl;
+    cout << "La posicion de memoria donde se guarda el numero es: " << puntero << endl; //La direccion de memoria donde se almacena el numero....
 
-    return 0;
+    return 0; //Programa principal retorna valor 0
 
 
 
