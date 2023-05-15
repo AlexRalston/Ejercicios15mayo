d0dca4760ebe9fda24adde5ca0604d83740e277d
AlexRalston <alejandroacedo@puenteuropa.com>
2023-05-15 11:04:52 +0200

    Array dinámico, numeros pares y memoria

--- arraysPares.cpp
+++ arraysPares.cpp
@@ -1,33 +1,33 @@
 #include<iostream>
 
 
-using namespace std;
+using namespace std;//Evitamos escribir std:: en la E/S de datos
 
 
 
-int main(){
+int main(){ //Programa principal
 
 
-int numeros[10];
-int* puntero = numeros;
+int* numeros =new int[10]; //Creamos un array dinámico de 10 posiciones
+int* puntero = numeros; //Asignamos una direccion de memoria a dicho vector
 
 
 
-for(int i=0; i<10;i++){
+for(int i=0; i<10;i++){ //Bucle que recorre el array añadiendo un valor introducido por el usuario a cada posicion
    cout<<"Ingrese un numero, por favor: ";
    cin>> *(puntero + i);
 
 }
 
-cout<<"Numeros pares encontrados: "<<endl;
+cout<<"Numeros pares encontrados: "<<endl; //Salida de datos
 
 
-for(int i=0; i<10;i++){
+for(int i=0; i<10;i++){ //Bucle que itera para buscar aquellos numeros que sean pares, es decir, cuya operacion modulo de como resultado 0
   if(*(puntero+i) % 2 == 0){
-    cout << "Numero par: " << *(puntero + i) << ", Posicion de memoria: " << (puntero + i) << endl;    
+    cout << "Numero par: " << *(puntero + i) << ", Posicion de memoria: " << (puntero + i) << endl;    //Salida de datos con el numero de par y la direccion de memoria asignada a dicho numero
   }
 }
- 
-return 0;
+delete[] numeros; //Escribimos delete para evitar fugas de memoria a posterior
+return 0; //El programa principal retorna valor 0
 
 }
